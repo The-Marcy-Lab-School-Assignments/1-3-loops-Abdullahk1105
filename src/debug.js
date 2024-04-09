6f81351652de31fd7122ee7ad70c70cfd1abd39a
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-04-08 22:17:16 -0400

    Assignment for loops was easy word(I struggled at the start)

--- src/debug.js
+++ src/debug.js
@@ -1,20 +1,28 @@
+// Question 8
 const brokenLoop = (start, end) => {
   let result = '';
-  for (let i = start; i < end; i += 2) {
+  for (let i = start; i <= end; i++) {
     result += i;
   }
   return result;
 };
+console.log(brokenLoop(1, 5));
+console.log(brokenLoop(1, 10));
+console.log(brokenLoop(0, 0));
+
+// Question 9
 
 const brokenNested = () => {
   let result = '';
   for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 10; i++) {
-      result += `-${i}${i}`;
+    for (let j = 0 ; j < 10; j++) {
+      result += `-${i}${j}`;
     }
   }
   return result.slice(1);
 };
+console.log(brokenNested());
+
 
 module.exports = {
   brokenLoop,
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,21 +1,68 @@
+// Question 1
+
 const loop0UpTo10 = () => {
+for(let i = 0; i < 10; i++){
+  console.log(i)
+}
 };
 
+loop0UpTo10()
+
+//  Question 2
+
 const loop5to10 = () => {
+  for(let i = 5; i <= 10; i++){
+    console.log(i)
+  }
 };
+
+loop5to10()
+
+// Question 3
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for(let i = 0;i < 10; i += 2){
+    console.log(i)
+  }
 };
+loopEvenNumbersUpTo10()
+
+// Question 4
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--){
+    console.log(i)
+  }
 };
+countdown5to0()
 
-const loopUpToNum = () => {
+// Question 5
+
+const loopUpToNum = (num) => {
+  for (let i = 0; i < num; i++)
+  console.log(i)
 };
+loopUpToNum(5)
+loopUpToNum(15)
+
 
 const fizzbuzz = () => {
+  for(let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("fizzbuzz")
+    }else if (i % 3 === 0){
+      console.log("fizz")
+    }else if( i % 5 === 0){
+      console.log("buzz")
+    }else {
+      console.log(i)
+    }
+  }
+
 };
+
+fizzbuzz()
 
 module.exports = {
   loop0UpTo10,
